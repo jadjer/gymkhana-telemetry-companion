85d3dd83f056d9e3beaa980b822bc589b29d0575
Pavel Suprunov <jadjer@192.168.100.3>
2024-07-02 13:30:35 +0300

    Initial commit

---
+++ app/src/main/java/by/jadjer/telemetry/ui/MainActivity.kt
@@ -0,0 +1,50 @@
+/*
+ * Copyright 2024 Pavel Suprunov
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package by.jadjer.telemetry.ui
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import by.jadjer.telemetry.ui.theme.GymkhanaTelemetryCompanionTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            GymkhanaTelemetryCompanionTheme {
+                TelemetryApp()
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun TelemetryPreview() {
+    GymkhanaTelemetryCompanionTheme {
+        TelemetryApp()
+    }
+}
